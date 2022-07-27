9604c517de36d07455772151d56e7347af68c09e
MahmoudBadawy4199 <m.badawy2011@gmail.com>
2022-07-28 01:08:29 +0200

    Card ui without responsive

--- src/app/modules/store-module/store-module.module.ts
+++ src/app/modules/store-module/store-module.module.ts
@@ -8,12 +8,13 @@ import { FormsModule } from '@angular/forms';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { ToggleButtonModule } from 'primeng/togglebutton';
 import { VirtualScrollerModule } from 'primeng/virtualscroller';
-
+import { CardModule } from 'primeng/card';
 import { StoreModuleRoutingModule } from './store-module-routing.module';
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductsComponent } from './components/products/products.component';
 import { CartComponent } from './components/cart/cart.component';
 import { WishlistComponent } from './components/wishlist/wishlist.component';
+import { ProductCardComponent } from './components/product-card/product-card.component';
 
 @NgModule({
   declarations: [
@@ -21,6 +22,7 @@ import { WishlistComponent } from './components/wishlist/wishlist.component';
     ProductsComponent,
     CartComponent,
     WishlistComponent,
+    ProductCardComponent,
   ],
   imports: [
     CommonModule,
@@ -33,6 +35,7 @@ import { WishlistComponent } from './components/wishlist/wishlist.component';
     InputTextareaModule,
     ToggleButtonModule,
     VirtualScrollerModule,
+    CardModule
   ],
 })
 export class StoreModuleModule {}
