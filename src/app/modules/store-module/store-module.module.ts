92a6108890351ba07fd3a98af86c0b523ed50b7e
Aswani20 <abdoaswani1997@gmail.com>
2022-07-29 01:43:52 +0200

    Add pagenation

--- src/app/modules/store-module/store-module.module.ts
+++ src/app/modules/store-module/store-module.module.ts
@@ -1,23 +1,27 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+
+//PrimeNg Component
 import { ImageModule } from 'primeng/image';
 import { RatingModule } from 'primeng/rating';
 import { ButtonModule } from 'primeng/button';
 import { CarouselModule } from 'primeng/carousel';
-import { FormsModule } from '@angular/forms';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { ToggleButtonModule } from 'primeng/togglebutton';
 import { VirtualScrollerModule } from 'primeng/virtualscroller';
 import { CardModule } from 'primeng/card';
 import { CheckboxModule } from 'primeng/checkbox';
-import { StoreModuleRoutingModule } from './store-module-routing.module';
+import { PaginatorModule } from 'primeng/paginator';
+
+//Components
 import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductsComponent } from './components/products/products.component';
 import { CartComponent } from './components/cart/cart.component';
 import { WishlistComponent } from './components/wishlist/wishlist.component';
 import { ProductCardComponent } from './components/product-card/product-card.component';
 import { FilterationComponent } from './components/filteration/filteration.component';
-
+import { StoreModuleRoutingModule } from './store-module-routing.module';
 
 @NgModule({
   declarations: [
@@ -40,7 +44,8 @@ import { FilterationComponent } from './components/filteration/filteration.compo
     ToggleButtonModule,
     VirtualScrollerModule,
     CardModule,
-    CheckboxModule
+    CheckboxModule,
+    PaginatorModule,
   ],
 })
 export class StoreModuleModule {}
