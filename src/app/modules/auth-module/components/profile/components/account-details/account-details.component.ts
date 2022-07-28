0d64e3111bd75434f1eaacbd72b3ae7885ca0b6f
Karim Mamdouh Mohamed <ghost_594@hotmail.com>
2022-07-29 00:02:47 +0200

    address and details added with validations

--- src/app/modules/auth-module/components/profile/components/account-details/account-details.component.ts
+++ src/app/modules/auth-module/components/profile/components/account-details/account-details.component.ts
@@ -1,15 +1,53 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-account-details',
   templateUrl: './account-details.component.html',
-  styleUrls: ['./account-details.component.scss']
+  styleUrls: ['./account-details.component.scss'],
 })
 export class AccountDetailsComponent implements OnInit {
+  showErrors: boolean = false; //Flag to show form errors
+  detailsForm: FormGroup = this._detailsBuilder.group({
+    firstName: [
+      '',
+      [Validators.required, Validators.pattern(/^[\S][A-Za-z]{2,}$/)],
+    ],
+    lastName: [
+      '',
+      [Validators.required, Validators.pattern(/^[\S][A-Za-z]{2,}$/)],
+    ],
+    password: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(
+          /^(((?=.*[a-z])(?=.*[A-Z]))|((?=.*[a-z])(?=.*[0-9]))|((?=.*[A-Z])(?=.*[0-9])))(?=.{6,})./
+        ), //At least one lowercase, at least one uppercase or one number, minimum 6 characters
+      ],
+    ],
+    userName: [
+      '',
+      [Validators.required, Validators.pattern(/^[\S][A-Za-z0-9]{5,}$/)],
+    ],
+    phoneNumber: ['', [Validators.required, Validators.minLength(11)]],
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get controlValidation() {
+    return this.detailsForm.controls;
   }
 
+  constructor(private _detailsBuilder: FormBuilder) {}
+
+  ngOnInit(): void {}
+  ngOnDestroy(): void {
+    this.detailsForm.reset();
+  }
+  submitDetailsForm(): void {
+    if (this.detailsForm.status === 'INVALID') {
+      this.showErrors = true;
+    } else {
+      console.log(this.detailsForm.value);
+    }
+  }
 }
--- src/app/modules/auth-module/components/profile/components/addresses/addresses.component.ts
+++ src/app/modules/auth-module/components/profile/components/addresses/addresses.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-addresses',
   templateUrl: './addresses.component.html',
-  styleUrls: ['./addresses.component.scss']
+  styleUrls: ['./addresses.component.scss'],
 })
 export class AddressesComponent implements OnInit {
+  showErrors: boolean = false; //Flag to show form errors
+  addressForm: FormGroup = this._addressForm.group({
+    city: ['', [Validators.required]],
+    area: ['', [Validators.required]],
+    street: ['', [Validators.required]],
+    buildingNumber: ['', [Validators.required]],
+    apartment: ['', [Validators.required]],
+    floor: ['', [Validators.required]],
+  });
 
-  constructor() { }
-
-  ngOnInit(): void {
+  get controlValidation() {
+    return this.addressForm.controls;
   }
 
+  constructor(private _addressForm: FormBuilder) {}
+
+  ngOnInit(): void {}
+  ngOnDestroy(): void {
+    this.addressForm.reset();
+  }
+  submitAddressForm(): void {
+    if (this.addressForm.status === 'INVALID') {
+      this.showErrors = true;
+    } else {
+      console.log(this.addressForm.value);
+    }
+  }
 }
