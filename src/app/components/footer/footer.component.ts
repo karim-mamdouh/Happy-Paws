cf2daaaa740065a163f6c201105b78bb03e43e8e
Karim Mamdouh Mohamed <ghost_594@hotmail.com>
2022-08-02 11:12:55 +0200

    Added footer

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -3,13 +3,26 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.scss']
+  styleUrls: ['./footer.component.scss'],
 })
 export class FooterComponent implements OnInit {
+  loggedIn: boolean = false; //Login flag for local storage
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    this.loggedIn = JSON.parse(localStorage.getItem('token')!);
   }
-
+  //Function to logout user by setting token to false and refreshing page
+  logout(): void {
+    localStorage.setItem('token', JSON.stringify(false));
+    if (window.location.href.includes('github')) {
+      let temp = window.location.href;
+      let base = temp.substring(0, temp.indexOf('io/') + 3);
+      let url = temp.replace(base, '');
+      window.location.href = base + url.substring(0, url.indexOf('/') + 1);
+    } else {
+      window.location.href = '/';
+    }
+  }
 }
